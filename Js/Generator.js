aca0cea44d0332a16dd1f0fbd1840bb5a011ba22
dawn475 <59984455+dawn475@users.noreply.github.com>
2026-05-05 18:54:58 -0400

    Update Generator.js

--- Js/Generator.js
+++ Js/Generator.js
@@ -1,1 +1,11 @@
 
+export function generateCat() {
+  return {
+    name: "TestCat",
+
+    appearance: {
+      baseColor: { name: "Gray", hex: "#6e6e6e" },
+      coatColor: { modifier: 1 }
+    }
+  };
+}
